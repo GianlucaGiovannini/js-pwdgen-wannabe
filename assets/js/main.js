133d7a96208369a19c1fc60d682d7af00c7e39d7
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-03-23 16:15:34 +0100

    Last Add new password

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,16 +3,28 @@
 
 
 // Chiedere all'utente il suo nome
-const pswName = prompt("scrivi il tuo nome?")
+const pswName = prompt("scrivi il tuo nome")
 console.log(pswName);
 
 // chiedere il suo cognome
-const pswLastName = prompt("scrivi il tuo cognome?")
+const pswLastName = prompt("scrivi il tuo cognome")
 console.log(pswLastName);
 
 // chiedere il suo colore preferito
-const pswColor = prompt("il tuo colore preferito ?")
+const pswColor = prompt("scrivi il tuo colore preferito")
 console.log(pswColor);
+
+// Chiedere all'utente il nome del suo migliore amico
+const pswFriendName = prompt("scrivi il nome del tuo migliore amico")
+console.log(pswFriendName);
+
+// chiedere il suo cognome
+const pswAnimal = prompt("scrivi un nome / tipo di animale")
+console.log(pswAnimal);
+
+// chiedere il suo colore preferito
+const pswAge = prompt("Quanti anni hai ?")
+console.log(pswAge);
 
 // stampare sulla pagina la password
 document.getElementById("password_complete").innerHTML = pswName + pswLastName + pswColor + 22;
@@ -22,6 +34,7 @@ console.log(password_complete)
 document.getElementById("psw2_complete").innerHTML = `00${pswName}11${pswLastName}22${pswColor}33`;
 console.log(psw2_complete)
 
+// stampare sulla pagina la password avanzata
 let psw1Name = "." + 00 + pswName + 22;
 console.log(psw1Name);
 
@@ -32,4 +45,10 @@ let psw1Color = pswColor + "stringa" + "./" + 22;
 console.log(psw1Color);
 
 document.getElementById("psw3_complete").innerHTML = `${psw1Name}${psw1LastName}${psw1Color}`;
+console.log(psw2_complete)
+
+
+// Stampare sulla pagina la password ESTREMA
+
+document.getElementById("psw4_complete").innerHTML = `${".!"}${pswName}${"/"}${pswLastName}30${pswColor}${"^"}${pswFriendName}087${pswAnimal}${"+"}${pswAge}`;
 console.log(psw2_complete)
